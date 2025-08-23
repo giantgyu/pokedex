513b660e36a8a5f968ff20f965c6debdbe6b63d3
giantgyu <jrmhfrancisco@gmail.com>
2025-08-23 13:16:05 +0800

    added trainers component

--- my-pokedex-app/src/app/app.component.ts
+++ my-pokedex-app/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./components/header/header.component";
-import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TrainersComponent } from './components/trainers/trainers.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, TrainersComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'my-pokedex-app';

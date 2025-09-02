19feb2622561f026dbecd4611b9d357c93e38a32
giantgyu <jrmhfrancisco@gmail.com>
2025-09-02 21:58:15 +0800

    Select Trainer

--- my-pokedex-app/src/app/components/trainers/trainers.component.ts
+++ my-pokedex-app/src/app/components/trainers/trainers.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../../dummy-users';
 
 @Component({
   selector: 'app-trainers',
   imports: [],
   templateUrl: './trainers.component.html',
-  styleUrl: './trainers.component.css'
+  styleUrl: './trainers.component.css',
 })
 export class TrainersComponent {
+  selectedTrainer = DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)];
 
+  get imagePath() {
+    return 'assets/users/' + this.selectedTrainer.avatar;
+  }
+
+  onSelectTrainer() {
+    alert('Select Trainer');
+  }
 }
---
+++ my-pokedex-app/src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Brock the Boulder', // Pewter City Gym Leader
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Misty the Waterflower', // Cerulean City Gym Leader
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Lt. Surge the Sparker', // Vermilion City Gym Leader
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'Erika the Thornmaster', // Celadon City Gym Leader
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Sabrina the Esper', // Saffron City Gym Leader
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Giovanni the Groundmaster', // Viridian City Gym Leader
+    avatar: 'user-6.jpg',
+  },
+];

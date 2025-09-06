e8a0f534b1784557875eccd6f56bfffc30a544eb
giantgyu <jrmhfrancisco@gmail.com>
2025-09-07 02:15:04 +0800

    Set up two way binding using ngmodel

--- my-pokedex-app/src/app/components/team-info/add-pokemon/add-pokemon.component.ts
+++ my-pokedex-app/src/app/components/team-info/add-pokemon/add-pokemon.component.ts
@@ -1,13 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-pokemon',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-pokemon.component.html',
   styleUrl: './add-pokemon.component.css',
 })
 export class AddPokemonComponent {
   @Output() cancel = new EventEmitter<void>();
+  enteredPokemon = '';
+  enteredPokedexEntry = '';
+  enteredType1 = '';
+  enteredType2 = '';
 
   onCancelClick() {
     this.cancel.emit();
